36041ebe7e8920ef63427e6936e81aa9a87e4468
Kevin <hellokevinvogel@gmail.com>
2022-04-10 22:57:05 +0200

    chore(command): add interceptor

--- src/main/java/com/example/productsservice/ProductsServiceApplication.java
+++ src/main/java/com/example/productsservice/ProductsServiceApplication.java
@@ -1,17 +1,34 @@
 package com.example.productsservice;
 
+import com.example.productsservice.command.interceptors.CreateProductCommandInterceptor;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProductsServiceApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(ProductsServiceApplication.class, args);
 		SpringApplication application = new SpringApplication(ProductsServiceApplication.class);
 		application.addListeners(new ApplicationPidFileWriter("./bin/app.pid"));
 		application.run(args);
 	}
+
+	@Autowired
+	public void registerCreateProductCommandInterceptor(ApplicationContext context,
+			CommandBus commandBus) {
+		commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
+	}
+
+	// @Autowired
+	// public void configure(EventProcessingConfigurer config) {
+	// config.registerListenerInvocationErrorHandler("product-group",
+	// conf -> new ProductsServiceEventsErrorHandler());
+	// }
 
 }
--- src/main/java/com/example/productsservice/query/ProductEventsHandler.java
+++ src/main/java/com/example/productsservice/query/ProductEventsHandler.java
@@ -4,16 +4,15 @@ import com.example.productsservice.core.data.ProductEntity;
 import com.example.productsservice.core.data.ProductsRepository;
 import com.example.productsservice.core.events.ProductCreatedEvent;
 
+import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@ProcessingGroup("product-group")
 public class ProductEventsHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProductEventsHandler.class);
     private final ProductsRepository productsRepository;
 
     public ProductEventsHandler(ProductsRepository productsRepository) {
